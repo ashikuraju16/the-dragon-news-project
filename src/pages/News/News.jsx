e03d25613895c4688163bb890ea968e995e91bad
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-06-12 21:05:48 +0600

    news

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,17 +1,22 @@
-
-
 const News = () => {
-    return (
-					<div className="mt-10 text-center bg-[#f3f3f3]">
-						<div className="">
-							<h2>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Aspernatur, ipsam sit veritatis odit rem unde inventore eveniet.
-								Officia, deserunt sunt.
-							</h2>
-						</div>
-					</div>
-				);
+	return (
+		<div className="mt-10 text-center bg-[#f3f3f3] w-4/6 mx-auto py-3 px-3 font-poppins">
+			<div className=" flex items-center justify-between">
+				<button
+					type="button"
+					className=" text-white text-xl bg-[#d72050] h-12 px-6 mr-1"
+				>
+					Latest
+				</button>
+				<h2 className=" font-medium line-clamp-1 text-xl">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus quas
+					ad at laboriosam laborum? Reprehenderit optio, nisi tenetur accusamus
+					atque quibusdam deserunt facere ipsam culpa reiciendis fuga vitae?
+					Voluptatum, maiores!30
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default News;
